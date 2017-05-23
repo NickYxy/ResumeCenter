42ba40764f07267dae5aa0954030adc0669722a0
Damnsure <bing900713@163.com>
2017-05-23 16:04:05 +0800

    增加邮件和简历推送功能

--- models/document.py
+++ models/document.py
@@ -1,217 +1,59 @@
-from usr_util.utils import *
-
-# mongodb config
-from config import config
-
-db = config.db
+from . import MongoModel
+from . import bool_dict
+from models.mail import send_document
+from flask import current_app as app
 
 
-def next_id(name):
-    query = {
-        'name': name,
-    }
-    update = {
-        '$inc': {
-            'seq': 1
-        }
-    }
-    kwargs = {
-        'query': query,
-        'update': update,
-        'upsert': True,
-        'new': True,
-    }
-    doc = db['data_id']
-    new_id = doc.find_and_modify(**kwargs).get('seq')
-    return new_id
-
-
-class MongoModel(object):
+class Document(MongoModel):
     @classmethod
     def _fields(cls):
         fields = [
-            '_id',
-            ('id', int, -1),
-            ('uuid', str, ''),
-            ('type', str, ''),
-            ('deleted', bool, False),
-            ('ct', int, 0),
-            ('ut', int, 0),
-        ]
-        return fields
-
-    '''
-class User(MongoModel):
-    @classmethod
-    def _fields(cls):
-        fields = [
-            ('name', str, ''),
-            ('password', str, ''),
+            ('user_uuid', str, ''),
+            ('doc_url', str, ''),
+            ('title', str, ''),
+            ('public', bool, False),
+            ('detail', str, ''),
+            ('css', str, ''),
         ]
         fields.extend(super()._fields())
         return fields
-    '''
 
     @classmethod
-    def has(cls, **kwargs):
-        return cls.find_one(**kwargs) is not None
-
-    def __repr__(self):
-        class_name = self.__class__.__name__
-        properties = ('{0} = {1}'.format(k, v) for k, v in self.__dict__.items())
-        return '<{0}: \n  {1}\n>'.format(class_name, '\n  '.join(properties))
-
-    @classmethod
-    def new(cls, form=None, **kwargs):
-        name = cls.__name__
-        m = cls()
-        fields = cls._fields()
-        fields.remove('_id')
-        if form is None:
-            form = {}
-
-        for f in fields:
-            k, t, v = f
-            if k in form:
-                setattr(m, k, t(form[k]))
-            else:
-                setattr(m, k, v)
-        for k, v in kwargs.items():
-            if hasattr(m, k):
-                setattr(m, k, v)
-            else:
-                raise KeyError
-        m.id = next_id(name)
-        ts = int(time.time())
-        m.ct = ts
-        m.ut = ts
-        m.type = name.lower()
-        m.set_uuid()
+    def new(cls, form):
+        m = super().new(form)
+        from uuid import uuid4
+        m.set_uuid(field='doc_url', seed=uuid4)
         m.save()
         return m
 
-    @classmethod
-    def _new_with_bson(cls, bson):
-        m = cls()
-        fields = cls._fields()
-        fields.remove('_id')
-        for f in fields:
-            k, t, v = f
-            if k in bson:
-                setattr(m, k, bson[k])
-            else:
-                setattr(m, k, v)
-        setattr(m, '_id', bson['_id'])
-        return m
+    def update_pic(self, pic):
+        allowed_type = ['jpg', 'jpeg', 'gif', 'png']
+        upload_name = pic.filename
+        if upload_name != '' and upload_name.split('.')[-1] in allowed_type:
+            path = app.config['USER_PIC_DIR']
+            ext = app.config['PRODUCT_PIC_EXT']
+            fullname = '{}{}.{}'.format(path, str(self.id), ext)
+            pic.save(fullname)
+            self.pic = '/' + fullname
+            self.save()
+        return self
 
-    @classmethod
-    def all(cls):
-        return cls.find()
+    def set_pic_url(self, url):
+        if len(url) > 0:
+            self.pic = url
+            self.save()
 
-    @classmethod
-    def find(cls, **kwargs):
-        name = cls.__name__
-        kwargs['deleted'] = kwargs.pop('deleted', False)
-        flag_sort = '__sort'
-        sort = kwargs.pop(flag_sort, None)
-        ds = db[name].find(kwargs)
-        if sort is not None:
-            ds = ds.sort(sort)
-        l = [cls._new_with_bson(d) for d in ds]
-        return l
+    def update_setting(self, form):
+        form = form.to_dict()
+        public = form.pop('public', 'false')
+        form['public'] = bool_dict.get(public, False)
+        if self.has(doc_url=form.get('doc_url')):
+            form.pop('doc_url')
+        self.update(form)
+        return self
 
-    @classmethod
-    def find_or(cls, args):
-        name = cls.__name__
-        search = {"$or": []}
-        for i in args:
-            i['deleted'] = i.pop('deleted', False)
-            search['$or'].append(i)
-        ds = db[name].find(search)
-        l = [cls._new_with_bson(d) for d in ds]
-        return l
-
-    @classmethod
-    def search_or(cls, form):
-        search = []
-        for k, v in form.items():
-            if len(v) > 0:
-                search.append({k: {'$regex': v, '$options': '$i'}})
-        if len(search) > 0:
-            return cls.find_or(search)
-        else:
-            return cls.all()
-
-    @classmethod
-    def get(cls, id):
-        can = isinstance(id, str) and id.isdigit()
-        if can == True:
-            id = int(id)
-        return cls.find_one(id=id)
-
-    @classmethod
-    def get_uuid(cls, uuid):
-        return cls.find_one(uuid=uuid)
-
-    @classmethod
-    def find_one(cls, **kwargs):
-        kwargs['deleted'] = kwargs.pop('deleted', False)
-        l = cls.find(**kwargs)
-        if len(l) > 0:
-            return l[0]
-        else:
-            return None
-
-    def save(self):
-        name = self.__class__.__name__
-        db[name].save(self.__dict__)
-
-    def delete(self):
-        name = self.__class__.__name__
-        query = {
-            'id': self.id,
-        }
-        values = {
-            '$set': {
-                'deleted': True,
-            },
-        }
-        db[name].update_one(query, values)
-
-    def update(self, form, hard=False):
-        for k, v in form.items():
-            if hard or hasattr(self, k):
-                setattr(self, k, v)
-        self.ut = timestamp()
-        self.save()
-
-    def blacklist(self):
-        b = [
-            '_id',
-        ]
-        return b
-
-    def json(self):
-        _dict = self.__dict__
-        d = {k: v for k, v in _dict.items() if k not in self.blacklist()}
-        return d
-
-    def data_count(self, cls):
-        name = cls.__name__
-        fk = '{}_id'.format(self.__class__.__name__.lower())
-        query = {
-            fk: self.id,
-        }
-        count = db[name].find(query).count()
-        return count
-
-    def set_uuid(self, field='uuid'):
-        new_uuid = short_uuid()
-        kwargs = {
-            field: new_uuid,
-        }
-        while self.__class__.has(**kwargs):
-            new_uuid = short_uuid()
-            kwargs[field] = new_uuid
-        setattr(self, field, new_uuid)
-        self.save()
+    def send_email(self, email):
+        import markdown2
+        detail_html = markdown2.markdown(self.detail)
+        html = '<div id="preview">{}</div><style>{}</style>'.format(detail_html, self.css)
+        send_document(email, self.title, html)
--- routes/user.py
+++ routes/user.py
@@ -1,5 +1,8 @@
 from routes import *
 from models.user import User
+from models.document import Document
+from models.auth import Auth
+from models.access import Access
 from decimal import Decimal
 from flask import current_app as app
 
@@ -10,6 +13,8 @@ Model = User
 
 @main.route('/login')
 def index():
+    if current_user() is not None:
+        return redirect(url_for('user.dashboard'))
     return render_template('user/login.html')
 
 
@@ -24,7 +29,7 @@ def login():
     u = User.find_one(username=username)
     if u is not None and u.validate_login(form):
         session['uid'] = u.id
-        return redirect(url_for('index.index'))
+        return redirect(url_for('user.dashboard'))
     else:
         flash('用户名密码错误', 'warning')
         return redirect(url_for('user.index'))
@@ -32,6 +37,8 @@ def login():
 
 @main.route('/register')
 def register_page():
+    if current_user() is not None:
+        return redirect(url_for('user.dashboard'))
     return render_template('user/register.html')
 
 
@@ -48,11 +55,33 @@ def register():
         u.send_email_verify(u.email)
         session['uid'] = u.id
         flash('验证邮件已发送，请查收', 'info')
-        return redirect(url_for('index.index'))
+        return redirect(url_for('user.dashboard'))
     else:
         for msg in msgs:
             flash(msg, 'warning')
         return redirect(url_for('user.register'))
+
+
+@main.route('/password/forget')
+def forget_password():
+    if current_user() is not None:
+        return redirect(url_for('user.dashboard'))
+    return render_template('user/forget_password.html')
+
+
+@main.route('/password/forget', methods=['POST'])
+def forget_password_send():
+    form = request.form
+    captcha = form.get('captcha', '').lower()
+    if captcha != session.get('captcha', 'no captcha!'):
+        flash('验证码错误', 'warning')
+        return redirect(url_for('user.forget_password'))
+    r = User.forget_password(form)
+    if r:
+        flash('密码重置邮件已经发送，请查收邮箱', 'success')
+    else:
+        flash('用户名或邮箱不匹配', 'warning')
+    return redirect(url_for('user.forget_password'))
 
 
 @main.route('/email/verify/<tb64>')
@@ -62,6 +91,30 @@ def email_verify(tb64):
     else:
         flash('邮箱验证失败', 'danger')
     return redirect(url_for('user.profile'))
+
+
+@main.route('/password/forget/verify/<tb64>')
+def forget_password_verify(tb64):
+    if User.forget_password_verify(tb64):
+        flash('重置邮件验证通过', 'success')
+        return render_template('user/reset_password.html', tb64=tb64)
+    else:
+        flash('重置邮件验证失败', 'danger')
+        return redirect(url_for('user.index'))
+
+
+@main.route('/password/reset/<tb64>', methods=['POST'])
+def reset_password(tb64):
+    password = request.form.get('password', '')
+    if User.forget_password_verify(tb64):
+        u = User.get_user_by_tb64(tb64)
+        u.reset_password(password)
+        session['uid'] = u.id
+        flash('密码已重置', 'success')
+        return redirect(url_for('user.profile'))
+    else:
+        flash('重置邮件验证失败', 'warning')
+        return redirect(url_for('user.index'))
 
 
 @main.route('/avatar/upload', methods=['POST'])
@@ -79,6 +132,164 @@ def logout():
     p = session.pop('uid')
     print('logout: pop uid', p)
     flash('账号已安全退出', 'success')
-    return redirect(url_for('index.index'))
+    return redirect(url_for('user.login'))
 
 
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    u = current_user()
+    return render_template('user/dashboard.html', u=u)
+
+
+@main.route('/profile')
+@login_required
+def profile():
+    u = current_user()
+    d = Document.find_one(user_uuid=u.uuid)
+    d.doc_url_full_path = app.config['DOC_URL'] + d.doc_url
+    return render_template('user/profile.html', u=u, d=d)
+
+
+@main.route('/profile', methods=['POST'])
+@login_required
+def profile_update():
+    cu = current_user()
+    form = request.form
+    cu.safe_update_user(form)
+    flash('信息已更新', 'success')
+    return redirect(url_for('user.profile'))
+
+
+@main.route('/email/update', methods=['POST'])
+@login_required
+def update_email():
+    u = current_user()
+    form = request.form
+    new_email = form.get('email', '')
+    captcha = form.get('captcha', '').lower()
+    if captcha != session.get('captcha', 'no captcha!'):
+        return json.dumps({'status': 'warning', 'msg': '验证码错误'})
+    if User.has(email=new_email) and User.find_one(email=new_email).uuid != u.uuid:
+        return json.dumps({'status': 'warning', 'msg': '该邮箱已被占用'})
+    if u.validate_login(form):
+        u.send_email_verify(new_email)
+        return json.dumps({'status': 'info', 'msg': '已发送验证邮件，请查收'})
+    else:
+        return json.dumps({'status': 'warning', 'msg': '密码错误'})
+
+
+@main.route('/pic/upload', methods=['POST'])
+@login_required
+def pic_upload():
+    u = current_user()
+    pic = request.files['pic']
+    pic = u.pic_upload(pic)
+    if pic is not False:
+        return json.dumps({'status': 'success', 'msg': '上传成功：' + pic, 'pic': pic})
+    else:
+        return json.dumps({'status': 'danger', 'msg': '上传失败'})
+
+
+@main.route('/pic/del', methods=['POST'])
+@login_required
+def pic_del():
+    u = current_user()
+    pic = request.json.get('pic')
+    u.pic_del(pic)
+    return json.dumps({'status': 'success', 'msg': '已删除：' + pic})
+
+
+@main.route('/document')
+@login_required
+def document():
+    u = current_user()
+    u.pics_url = [app.config['BASE_URL'] + app.config['USER_PIC_DIR'] + pic for pic in u.pics]
+    u.pics_url.reverse()
+    d = Document.find_one(user_uuid=u.uuid)
+    d.doc_url_full_path = app.config['DOC_URL'] + d.doc_url
+    return render_template('user/document.html', p=d, u=u)
+
+
+@main.route('/document', methods=['POST'])
+@login_required
+def document_update():
+    u = current_user()
+    p = Document.find_one(user_uuid=u.uuid)
+    form = request.form
+    p.update(form)
+    return redirect(url_for('user.document'))
+
+
+@main.route('/document/setting', methods=['POST'])
+@login_required
+def document_setting():
+    u = current_user()
+    p = Document.find_one(user_uuid=u.uuid)
+    form = request.form
+    p.update_setting(form)
+    flash('简历设置已更新', 'success')
+    return redirect(url_for('user.profile'))
+
+
+@main.route('/auths')
+@login_required
+@email_verify_required
+def auths():
+    u = current_user()
+    auths = Auth.find(user_uuid=u.uuid)
+    auths.reverse()
+    for a in auths:
+        a.verified = a.verify()
+    return render_template('user/auths.html', u=u, auths=auths)
+
+
+@main.route('/auths', methods=['POST'])
+@login_required
+def auths_search():
+    u = current_user()
+    form = request.form
+    auths = Auth.search_or(form)
+    auths.reverse()
+    for a in auths:
+        a.verified = a.verify()
+    return render_template('user/auths.html', u=u, auths=auths)
+
+
+@main.route('/auth/new')
+@login_required
+@email_verify_required
+def auth_new():
+    u = current_user()
+    d = Document.find_one(user_uuid=u.uuid)
+    form = dict(
+        user_uuid=u.uuid,
+        doc_uuid=d.uuid,
+    )
+    a = Auth.new(form)
+    return redirect(url_for('user.auth', token=a.token))
+
+
+@main.route('/auth/<token>')
+@login_required
+@email_verify_required
+def auth(token):
+    u = current_user()
+    d = Document.find_one(user_uuid=u.uuid)
+    d.base_url = app.config['BASE_URL'][:-1]
+    a = Auth.find_one(token=token)
+    a.verified = a.verify()
+    history = Access.find(token=token, user_uuid=u.uuid)
+    history.reverse()
+    return render_template('user/auth.html', u=u, d=d, a=a, history=history)
+
+
+@main.route('/auth/<token>', methods=['POST'])
+@login_required
+def auth_update(token):
+    u = current_user()
+    a = Auth.find_one(token=token)
+    form = request.form
+    if a.user_uuid == u.uuid:
+        a.update_setting(form)
+    return redirect(url_for('user.auth', token=token))
